4ca52fe9dad6169db189538fdf469d50655ee536
Aleksey Volgankin <volgankin@bk.ru>
2021-06-05 13:02:06 +0300

    Выполнено третье задание

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
     player: 1,
     name: 'Китана',
     hp: 100,
+    ks: 20,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['Боевые веера'],
     attack: function () {
@@ -15,6 +17,7 @@ const player2 = {
     player: 2,
     name: 'Соня',
     hp: 100,
+    ks: 25,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['Бамбуковые палки'],
     attack: function () {
@@ -51,6 +54,50 @@ function createPlayer(playerInfo) {
 
     return player;
 };
+
+function getLoseHP (maxKickStrong) {
+    let loseHP = Math.ceil(Math.random() * maxKickStrong)
+
+    return loseHP;
+};
+
+function changeHP (player) {
+    const $playerLife = document.querySelector('.player' + player.player + ' .life');
+    player.hp -= getLoseHP(player.ks);
+    $playerLife.style.width = player.hp + '%';
+
+    if (player.hp <= 0) {
+        player.hp = 0;
+        $randomButton.disabled = true;
+        $randomButton.style.backgroundColor = 'grey';
+    }
+
+    return player.hp;
+};
+
+function playerWins (name) {
+    const $winsTitle = createTag('div', 'winsTitle');
+    $winsTitle.innerText = name + ' wins';
+
+    return $winsTitle;
+};
+
+function getWinner (hpPlayer1, hpPlayer2) {
+    if (hpPlayer1 <= 0 || hpPlayer2 <= 0) {
+        if (hpPlayer1 > hpPlayer2) {
+            $arenas.appendChild(playerWins(player1.name));
+        } else {
+            $arenas.appendChild(playerWins(player2.name));
+        }
+    }
+};
+
+$randomButton.addEventListener('click', function () {
+    const letHPPlayer1 = changeHP(player1);
+    const letHPPlayer2 = changeHP(player2);
+
+    getWinner(letHPPlayer1, letHPPlayer2);
+});
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
