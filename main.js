36be6432f321ba358fb60d4c632c27fcf75b5d21
Aleksey Volgankin <volgankin@bk.ru>
2021-06-10 16:37:45 +0300

    Выполнил пятое задание

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const $arenas = document.querySelector('.arenas');
-/*const $randomButton = document.querySelector('.button');*/
+const $randomButton = document.querySelector('.button');
 const $formFight = document.querySelector('.control');
 
 const HIT = {
@@ -114,19 +114,21 @@ function createReloadButton () {
     $arenas.appendChild($reloadWrap);
 };
 
-/*$randomButton.addEventListener('click', function () {
-    player1.changeHP(getRandom(20));
-    player1.renderHP();
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
 
-    player2.changeHP(getRandom(20));
-    player2.renderHP();
+function enemyAttack () {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
 
-    if (player1.hp === 0 || player2.hp === 0) {
-        $randomButton.disabled = true;
-        $randomButton.style.backgroundColor = 'grey';
-        createReloadButton();
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence,
     }
+};
 
+function renderMessage () {
     if (player1.hp === 0 && player1.hp < player2.hp) {
         $arenas.appendChild(playerWins(player2.name));
     } else if (player2.hp === 0 && player2.hp < player1.hp) {
@@ -134,20 +136,54 @@ function createReloadButton () {
     } else if (player1.hp === 0 && player2.hp === 0) {
         $arenas.appendChild(playerWins());
     }
-});*/
+};
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
+function fightButtonOff () {
+    if (player1.hp === 0 || player2.hp === 0) {
+        $randomButton.disabled = true;
+        $randomButton.style.backgroundColor = 'grey';
+        createReloadButton();
+    }
+};
 
-function enemyAttack () {
-    const hit = ATTACK[getRandom(3) - 1];
-    const defence = ATTACK[getRandom(3) - 1];
-    console.log('####: hit = ' + hit);
-    console.log('####: defence = ' + defence);
+function fight (enemy, player) {
+    if (enemy.hit !== player.defence) {
+        player2.changeHP(enemy.value);
+        player2.renderHP();
+    }
+
+    if (player.hit !== enemy.defence) {
+        player1.changeHP(player.value);
+        player1.renderHP();
+    }
+};
+
+function playerAttack () {
+    const attack = {};
+
+    for (let item of $formFight) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value;
+        }
+
+        if (item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+
+        item.checked = false;
+    }
+
+    return attack;
 };
 
 $formFight.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log($formFight);
-    enemyAttack();
+
+    const enemy = enemyAttack();
+    const player = playerAttack();
+
+    fight(enemy, player);
+    fightButtonOff();
+    renderMessage();
 });
