76de8baa278b1d1e6eee8bca2560f60a36ad019e
Aleksey Volgankin <volgankin@bk.ru>
2021-06-16 11:28:57 +0300

    Исправление ошибки при ничье

--- modules/generateLogs.js
+++ modules/generateLogs.js
@@ -3,9 +3,6 @@ import { $chat } from './const.js';
 import { getRandom } from './getRandom.js';
 
 function generateLogs (type, player1, player2, value) {
-    const { name: name1 } = player1;
-    const { name: name2, hp: hp2 } = player2;
-
     const date = new Date();
     const time = date.getHours() + ':' + date.getMinutes();
     let text = '';
@@ -13,19 +10,19 @@ function generateLogs (type, player1, player2, value) {
 
     switch (type) {
         case 'start':
-            text = logs[type].replace('[time]', time).replace('[player1]', name1).replace('[player2]', name2);
+            text = logs[type].replace('[time]', time).replace('[player1]', player1.name).replace('[player2]', player2.name);
             el = `<p>${text}</p>`;
             break;
         case 'end':
-            text = logs[type][getRandom(logs[type].length) - 1].replace('[time]', time).replace('[playerWins]', name1).replace('[playerLose]', name2);
+            text = logs[type][getRandom(logs[type].length) - 1].replace('[time]', time).replace('[playerWins]', player1.name).replace('[playerLose]', player2.name);
             el = `<p>${text}</p>`;
             break;
         case 'hit':
-            text = logs[type][getRandom(logs[type].length) - 1].replace('[playerKick]', name1).replace('[playerDefence]', name2);
-            el = `<p>${time} - ${text} -${value} [${hp2}/100]</p>`;
+            text = logs[type][getRandom(logs[type].length) - 1].replace('[playerKick]', player1.name).replace('[playerDefence]', player2.name);
+            el = `<p>${time} - ${text} -${value} [${player2.hp}/100]</p>`;
             break;
         case 'defence':
-            text = logs[type][getRandom(logs[type].length) - 1].replace('[playerKick]', name1).replace('[playerDefence]', name2);
+            text = logs[type][getRandom(logs[type].length) - 1].replace('[playerKick]', player1.name).replace('[playerDefence]', player2.name);
             el = `<p>${time} - ${text}</p>`;
             break;
         case 'draw':
