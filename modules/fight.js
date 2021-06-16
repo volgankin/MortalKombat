c9ad1573ee59c0567aaf84bb8ba659390d3f607f
Aleksey Volgankin <volgankin@bk.ru>
2021-06-16 11:04:39 +0300

    Добавил деструктуризацию в функцию fight

--- modules/fight.js
+++ modules/fight.js
@@ -2,18 +2,21 @@ import { player1,  player2 } from './players.js';
 import generateLogs from './generateLogs.js';
 
 function fight (enemy, player) {
-    if (enemy.hit !== player.defence) {
-        player2.changeHP(enemy.value);
+    const { value: vEnemy, hit: hitEnemy, defence: defEnemy } = enemy;
+    const { value: vPlayer, hit: hitPlayer, defence: defPlayer } = player;
+
+    if (hitEnemy !== defPlayer) {
+        player2.changeHP(vEnemy);
         player2.renderHP();
-        generateLogs('hit', player1, player2, enemy.value);
+        generateLogs('hit', player1, player2, vEnemy);
     }  else {
         generateLogs('defence', player1, player2);
     }
 
-    if (player.hit !== enemy.defence) {
-        player1.changeHP(player.value);
+    if (hitPlayer !== defEnemy) {
+        player1.changeHP(vPlayer);
         player1.renderHP();
-        generateLogs('hit', player2, player1, player.value);
+        generateLogs('hit', player2, player1, vPlayer);
     } else {
         generateLogs('defence', player2, player1);
     }
